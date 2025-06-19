0e7e06204011d5c42ed67b5e72c26801ab39cf7f
Soraiya11-7 <soraiyaparvin@gmail.com>
2025-06-19 13:14:01 +0600

    Vuejs Integrations section added on home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,12 @@
+import VueJsIntegrations from "../../components/VueJsIntegrations";
+
 const Home = () => {
-  return <div>home</div>;
+  return <div>home
+
+
+{/*  Vue js Framework Integrations and Features*/}
+    <VueJsIntegrations/>
+  </div>;
 };
 
 export default Home;
